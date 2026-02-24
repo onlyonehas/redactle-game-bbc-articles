500f0bd1f1582b19a27c8a00c1429d1226679044
onlyonehas <hassanabbas93@hotmail.com>
2026-02-24 14:41:05 +0000

    Update Vite base path for deployment to reflect new game URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/redactle-for-bbc/',
+    base: '/redactle-game-bbc-articles/',
     server: {
         proxy: {
             '/news': {
